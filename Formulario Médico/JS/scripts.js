13827b30befa870519d09c6d9dc31072e10a2221
Giovana Coutinho <ctnho1999@gmail.com>
2024-06-10 23:20:59 -0300

    finish atv

--- Formulario Médico/JS/scripts.js
+++ Formulario Médico/JS/scripts.js
@@ -1,3 +1,90 @@
+function limpaFormulario(){
+    document.getElementById('formulario').reset();
+}
+
+function validacaoEmail(inputEmail){
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!regex.test(inputEmail)) {
+        alert("E-mail está incorreto. Por favor, insira um e-mail válido.");
+        document.getElementById("campo_nome").focus();
+    }
+}
+
+function validarTodosInput(){
+    var inputs = document.querySelectorAll('input[required]');
+    for (var i = 0; i < inputs.length; i++){
+        if(inputs[i].value === ""){
+            alert("Por favor, preencha todos os campos.");
+            return false;
+        }
+    }
+    return true;
+}
+
+function validarTipagem(){
+    var inputs = document.querySelectorAll('input[type="text"], select');
+    var erro = false;
+    var mensagemErro = "";
+
+    inputs.forEach(function(input){
+        var id = input.id;
+        var tipo = input.type;
+        var valor = input.value;
+
+        switch(tipo){
+            case "text":
+                if (!valor.match(input.pattern)){
+                    erro = true;
+                    mensagemErro = "Verifique as informações digitadas e tente novamente\n";
+                }
+                break;
+            default:
+                break;
+        }
+    });
+    if(erro){
+        alert(mensagemErro);
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validarPatterns(){
+    var inputs = document.querySelectorAll('input[pattern], select');
+    var erro = false;
+    var mensagemErro = "";
+
+    inputs.forEach(function(input){
+        var pattern = input.getAttribute("pattern");
+        var valor = input.value;
+
+        if(valor === "" || (pattern && !new RegExp("^" + pattern + "$").test(valor))){
+            var label = input.previousElementSibling.textContent.trim().replace(/:$/, "");
+            mensagemErro += "O valor inserido para '" + label + "' não está correto.\n";
+            erro = true;
+        }
+    });
+    if(erro){
+        alert(mensagemErro);
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validarFormulario(){
+    var tipagemValida = validarTipagem();
+    var todosInputsPreenchidos = validarTodosInput();
+    var validacaoPattern = validarPatterns();
+
+    if(todosInputsPreenchidos === false){
+        return todosInputsPreenchidos;
+    }else{
+        return tipagemValida && validacaoPattern;
+    }
+}
+
 function limpa_formulário_cep() {
     document.getElementById('rua').value = "";
     document.getElementById('bairro').value = "";
